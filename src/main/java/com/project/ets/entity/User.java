b8b5f58a94025c9582fb34d9b8c306ddf5f934d9
Sathishbabu36 <sathish03ece@gmail.com>
2024-08-27 12:13:50 +0530

    logs added

---
+++ src/main/java/com/project/ets/entity/User.java
@@ -0,0 +1,45 @@
+package com.project.ets.entity;
+
+import java.time.LocalDateTime;
+
+import com.project.ets.config.GenerateSequenceId;
+import com.project.ets.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+	@Id
+    @Column(name = "user_id")
+	@GenerateSequenceId
+	private String userId;
+	
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "role")
+	private UserRole role;
+	
+	@Column(name = "createdDate")
+	private LocalDateTime createdDate;
+	
+	@Column(name = "modifiedDate")
+	private LocalDateTime modifiedDate;
+}
